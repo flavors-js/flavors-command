2362de0837d39ec79752d44043e51357e0f8bd79
Michael Ledin <m.ledin@appheads.ru>
2017-11-15 05:31:18 +0300

    fix(*): pass args.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = flavorsOptions => {
   let command = require('flavors')(flavorsOptions.configName,
-    Object.assign({}, flavorsOptions, {configFileName: 'command'}));
+    Object.assign({}, flavorsOptions, { configFileName: 'command' }));
 
   let argIndex = 2;
   for (; argIndex < process.argv.length && typeof command === 'object' && command.command === undefined; argIndex++) {
@@ -12,40 +12,43 @@ module.exports = flavorsOptions => {
   const args = process.argv.slice(argIndex);
 
   if (typeof command === 'function') {
-    command(...args);
+    command = command(...args);
+    if (command === undefined || command.command === undefined) {
+      return;
+    }
+  }
+
+  if (command === undefined) {
+    command = {
+      command: process.argv[2],
+      args: process.argv.slice(3)
+    };
   } else {
-    if (command === undefined) {
-      command = {
-        command: process.argv[2],
-        args: process.argv.slice(3)
-      };
-    } else {
-      if (typeof command === 'object' && typeof command.command === 'function') {
-        command = command.command;
-      } else if (args.length > 0) {
-        if (typeof command === 'string') {
-          command = {
-            command,
-            args
-          };
-        } else if (typeof command === 'object') {
-          command.args = command.args || [];
-          command.args.push(...args);
-        }
+    if (typeof command === 'object' && typeof command.command === 'function') {
+      command = command.command;
+    } else if (args.length > 0) {
+      if (typeof command === 'string') {
+        command = {
+          command,
+          args
+        };
+      } else if (typeof command === 'object') {
+        command.args = command.args || [];
+        command.args.push(...args);
       }
     }
-
-
-    const child = require('flavors-runner')(Object.assign({}, flavorsOptions, {
-      command: {command},
-      spawnOptions: {
-        stdio: 'inherit'
-      }
-    }));
-
-    // to make Ctrl-C work
-    process.on('SIGINT', () => {
-      child.kill('SIGINT');
-    });
   }
+
+
+  const child = require('flavors-runner')(Object.assign({}, flavorsOptions, {
+    command: { command },
+    spawnOptions: {
+      stdio: 'inherit'
+    }
+  }));
+
+  // to make Ctrl-C work
+  process.on('SIGINT', () => {
+    child.kill('SIGINT');
+  });
 };
