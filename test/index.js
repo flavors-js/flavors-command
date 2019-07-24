162131ff8355f59da3d717e7d8583b1d5806c1b1
Michael Ledin <m.ledin@appheads.ru>
2019-07-24 11:21:24 +0300

    [skip release]: optimize tests.

--- test/index.js
+++ test/index.js
@@ -11,44 +11,40 @@ function testPath(...names) {
   return path.resolve(__dirname, ...names);
 }
 
-function outputEquals(args, options, expected) {
-  assert.strictEqual(child.execFileSync(`${cliPath}`, args, options).toString(), expected + '\n');
-}
-
-function options(dir, env = {}) {
-  return {
+function outputEquals(expected, args, dir, env = {}) {
+  assert.strictEqual(child.execFileSync(`${cliPath}`, args, {
     cwd: testPath(dir),
     env: Object.assign({}, process.env, env)
-  };
+  }).toString(), expected + '\n');
 }
 
 describe('flavors-command', () => {
   it('uses options from flavors options file: ' + flavorsCommand.optionsFile, () => {
-    outputEquals(['echo', '$value'], options('flavorsOptions'), '1');
+    outputEquals('1', ['echo', '$value'], 'flavorsOptions');
   });
 
   it('uses options from local flavors options file: ' + flavorsCommand.localOptionsFile, () => {
-    outputEquals(['echo', '$value1$value2'], options('localFlavorsOptions'), '12');
+    outputEquals('12', ['echo', '$value1$value2'], 'localFlavorsOptions');
   });
 
   it('uses custom flavors options file', () => {
     const testName = 'customFlavorsOptions';
-    outputEquals(['echo', '$value'], options(testName, {FLAVORS_OPTIONS_PATH: testPath(testName, testName + '.js')}), '1');
+    outputEquals('1', ['echo', '$value'], testName, {FLAVORS_OPTIONS_PATH: testPath(testName, testName + '.js')});
   });
 
   it('uses custom flavors local options file', () => {
     const testName = 'customFlavorsLocalOptions';
-    outputEquals(['echo', '$value1$value2'], options(testName, {FLAVORS_LOCAL_OPTIONS_PATH: testPath(testName, 'customFlavorsOptions.local.js')}), '12');
+    outputEquals('12', ['echo', '$value1$value2'], testName, {FLAVORS_LOCAL_OPTIONS_PATH: testPath(testName, 'customFlavorsOptions.local.js')});
   });
 
   it('uses custom flavors config name', () => {
-    outputEquals(['echo', '$value'], options('customConfigName', {FLAVORS_CONFIG_NAME: 'custom'}), '1');
+    outputEquals('1', ['echo', '$value'], 'customConfigName', {FLAVORS_CONFIG_NAME: 'custom'});
   });
 
   it('uses custom flavors options directory', () => {
     it('uses options from flavors options file', () => {
       const optionsDir = testPath('customFlavorsOptionsDir');
-      outputEquals(['echo', '$value'], options('', {FLAVORS_OPTIONS_PATH: optionsDir, FLAVORS_LOCAL_OPTIONS_PATH: optionsDir}), '1');
+      outputEquals('1', ['echo', '$value'], '', {FLAVORS_OPTIONS_PATH: optionsDir, FLAVORS_LOCAL_OPTIONS_PATH: optionsDir});
     });
   });
 });
